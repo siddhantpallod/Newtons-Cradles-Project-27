6f57395a1db35d78dd165611a4dad107736426ff
siddhantpallod <68698846+siddhantpallod@users.noreply.github.com>
2020-08-24 11:08:16 +0530

    Add files via upload

--- bob.js
+++ bob.js
@@ -1,26 +1,36 @@
-class Bob{
+class bob
+{
+	constructor(x,y,r)
+	{
+		var options = {
+			isStatic:false,
+			restitution:1,
+			friction:0,
+			density:0.8
+			
+			}
+		this.x = x;
+		this.y = y;
+		this.r = r
+		
+		this.body = Bodies.circle(this.x, this.y, (this.r)/2, options)
+		World.add(world, this.body);
 
-    constructor(x,y){
-        
-        var options = {
-            isStatic : false,
-            restitution : 0.5,
-            density : 1
-        } 
-        
-        this.x = x;
-        this.y = y;
-        this.body = Bodies.circle(this.x,this.y,20,options);
-        World.add(world,this.body);
-    }
+	}
+	display()
+	{
+			
+			var paperpos = this.body.position;
+			push()
+			translate(paperpos.x, paperpos.y);
+			ellipseMode(CENTER)
+			//strokeWeight(4);
+			fill(255,0,255)
+			//imageMode(CENTER);
+			//image(this.image, 0,0,this.r, this.r)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
 
-    display(){
-        var pos = this.body.position;
-        push();
-        translate(pos.x,pos.y);
-        ellipseMode(RADIUS);
-        fill("purple");
-        ellipse(0,0,20,20);
-        pop();
-    }
 }
